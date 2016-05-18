e05493ace6acdbdbf448d16056d75344acce5706
Sandro De Cristofaro <sandrodecristofaro@sandros-imac.attlocal.net>
2016-05-17 21:13:47 -0400

    - Changed a single task to a list of tasks - Added event handler and reference to selectedTask in lnAngularApp.js file - Styling a lil' bit lil' bit

--- app/lnAngularApp.js
+++ app/lnAngularApp.js
@@ -5,15 +5,28 @@
 	angular.module('lnAngularApp', []).controller('lnAngularAppCtrl', function() {
 		var ctrl = this;
 
-		ctrl.pageTitle = 'Task Info';
-
+		ctrl.pageTitle = 'My Task List';
 		ctrl.editorTitle = 'Task Editor';
+		ctrl.selectedTask = null;
 
-		ctrl.task = {
-			id: 1,
-			title: 'Make a demo',
-			description: 'Make a lunch and learn demo!'
-		};
+		ctrl.tasks = [
+			{ id: 1, title: 'Make espresso', description: 'Quick!' },
+			{ id: 2, title: 'Make breakfast', description: 'Bacon, Egg & cheese  sandwich!' },
+			{ id: 3, title: 'Go for a run', description: 'Put one foot in front of the other' },
+			{	id: 4, title: 'Go to the store', description: 'Buy more veggies' },
+			{	id: 5, title: 'Water flowers', description: 'They are getting droopy!' },
+			{	id: 6, title: 'Make lunch', description: 'Something light' },
+			{	id: 7, title: 'Paint a masterpiece', description: '\'Cause that\'s just how I do...' },
+			{	id: 8, title: 'Go to the mall', description: 'Buy present for nephews' },
+			{	id: 9, title: 'Call parents', description: 'Give yourself at least 45min...' },
+			{	id: 10, title: 'Make dinner', description: 'You have guests coming' },
+			{	id: 11, title: 'Rent a movie', description: 'Time to relax' }
+		];
+
+		ctrl.onSelectTask = function(task) {
+			ctrl.selectedTask = task;
+		}
+
 	});
 
 }(window.angular));
